c51a47b19be756a100267d6e6ebdaa0f7828f221
HelenCz <helen@czioska.eu>
2014-12-23 15:10:59 +0100

    Sum colums
    getting data ready for analysis

--- R/jenaanalysis.R
+++ R/jenaanalysis.R
@@ -1,13 +1,35 @@
+install.packages("lme4")
+install.packages("nlme")
+install.packages("MASS")
+library (lme4)
+library (MASS)
+
 jena <- read.csv2("C:/Users/hczioska/Documents/Data/data1.csv", header=T, sep= ";")
 names (jena)
 str(jena)
 
-###
+###adjustments
 jena$PatchID <- as.factor(jena$PatchID)
 jena$Instead <- as.factor(jena$Instead)
 jena$PatchID <- as.factor(jena$PatchID)
 
-jena$date <- as.POSIXct(paste(jena$Year, jena$Month, jena$Day, sep=" "), format="%Y %m %d") # Datum in R-Format
+jena$Date <- as.POSIXct(paste(jena$Year, jena$Month, jena$Day, sep=" "), format="%Y %m %d") # Datum in R-Format
+
+
+jena$Sum_all_visits <- jena$Bees_spec + jena$Bees_other + jena$Bombus_spec + jena$Bombus_other + jena$Hoverfly_spec +jena$Hoverfly_other + jena$Rest_spec + jena$Rest_other
+
+jena$Sum_spec_visits <- jena$Bees_spec  + jena$Bombus_spec  + jena$Hoverfly_spec  + jena$Rest_spec
+
+jena$Spec_visit_share <- jena$Sum_spec_visits/jena$Sum_all_visits
+
+jena$VR_spec <- jena$Sum_spec_visits/jena$Number_Flowers
+
+##############
+
+str(jena)
+
+length(unique(jena$Date)) #15 Sampling-Tage
+
 
 
 # Normal distribution?
@@ -15,27 +37,20 @@ jena$date <- as.POSIXct(paste(jena$Year, jena$Month, jena$Day, sep=" "), format=
 par(mfrow=c(2,2))
 qqnorm(jena$VR_spec)
 qqline(jena$VR_spec)
+shapiro.test(jena$VR_spec) #p-value = 2.2e-16 , not normally distributed
 hist(jena$VR_spec) #poisson
-shapiro.test(jena$VR_spec) #p-value = 2.2e-16
-
-hist(jena$Number_Flowers)
 
 # Boxplots
 par(mfrow=c(1,1))
-plot(VR_spec~Freq_Plot, data=jena, main="VR~Frequency", col= Spec, ylab="VR per Flower", xlab="Frequency in %", cex.axis=0.6)
-legend("topleft",species,col=1:length(jena$Spec),pch=1, cex=0.7)
+plot(VR_spec~Freq_Plot, data=jena, main="VR~Frequency Plot", col= Spec, ylab="VR per Flower", xlab="Frequency in %", cex.axis=0.6)
 
+plot(VR_spec~Freq_Patch, data=jena, main="VR~Frequency Patch", col= Spec, ylab="VR per Flower", xlab="Frequency in %", cex.axis=0.6)
 
-#####################
-##GLM################
+plot(VR_spec~Cover_Plot, data=jena, main="VR~Cover", col= Spec, ylab="VR per Flower", xlab="Cover in %", cex.axis=0.6)
 
-model1<-glm(VR_spec~Freq_Plot, data=jena)
-summary(model1) #0.00041 ***
+plot(VR_spec~SR_Plot, data=jena, main="VR~SR", col= Spec, ylab="VR per Flower", cex.axis=0.6)
 
-
-model1<-glm(VR_spec~Freq_Plot+Spec, data=jena)
-summary(model1, correlation=T ) 
-
+plot(VR_spec~SR_no_sinlges, data=jena, main="VR~SR > 1", col= Spec, ylab="VR per Flower",  cex.axis=0.6)
 
 
 
@@ -57,3 +72,13 @@ for (i in species){
   yfreq <- predict(model,list(Freq_Plot=xfreq), type="response")
   lines (xfreq, yfreq, col=j)
 }
+
+
+#####################
+##lme################
+
+model1<-glmmPQL( VR_spec ~ Freq_Plot, random = ~1|Spec, family=poisson, data=jena)
+summary(model1) #0.00041 ***
+
+
+
